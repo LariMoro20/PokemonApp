58627d04c48f67a086be710ba2d6810b2ae43dae
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-04-04 22:11:17 -0300

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,27 +7,6 @@ const routes = [{
         component: () =>
             import('src/pages/PokemonPage.vue')
     },
-    {
-        path: 'about',
-        component: () =>
-            import('src/pages/About.vue')
-    },
-    {
-        path: 'pokemons',
-        component: () =>
-            import('src/pages/PokemonPage.vue')
-    },
-    {
-        path: 'notes',
-        component: () =>
-            import('src/pages/Anotations.vue')
-    },
-    {
-        path: 'test',
-        component: () =>
-            import('src/pages/PokemonPage2.vue')
-    }
-
     ]
 }]
 
